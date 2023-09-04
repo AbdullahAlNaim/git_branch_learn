e99dfc67b7779e50200e4cc73395d37091f42cdb
AbdullahAlNaim <aanaim96@gmail.com>
2023-09-04 18:35:29 -0400

    some git commands to remember

---
+++ play.py
@@ -0,0 +1,16 @@
+# "git config --global user.name 'username'"
+# "git config --global user.email 'email'"
+# "git log --oneline" = gives one line list of commits
+# "git branch 'branchName'" = creates the new branch and gives name  
+# "git branch -a" show all branch lists including main branch
+# "git checkout 'branchName'" = switch to whichever branch mentioned
+# "git branch -a" = again to see list but branch on is highlighted
+# "git branch -d 'branchName'" = will delete the branch you're on only if commited
+# "git branch -D 'branchName'" = will delete branch without ever committing"
+# "git checkout -b 'branchName'" = will create a new branch and switch to it as well"
+# "git merge 'branchName'" = will merge the the branch to whichever branch you're on(be on main branch if need it there)"
+
+num = ['one', 'two']
+
+for x in num:
+    print(x)
